56588d81dd8b969ba607ed31978085a5df75c48e
Ayrton Douglas Freitas Guimarães <ayrtondouglas@usp.br>
2022-11-07 10:13:11 -0300

    feat: Foi implementado o laravel-usp-theme

---
+++ config/laravel-usp-theme.php
@@ -0,0 +1,36 @@
+<?php
+
+$menu = [
+];
+
+$right_menu = [
+];
+
+
+return [
+    # valor default para a tag title, dentro da section title.
+    # valor pode ser substituido pela aplicação.
+    'title' => config('app.name'),
+
+    # USP_THEME_SKIN deve ser colocado no .env da aplicação 
+    'skin' => env('USP_THEME_SKIN', 'uspdev'),
+
+    # chave da sessão. Troque em caso de colisão com outra variável de sessão.
+    'session_key' => 'laravel-usp-theme',
+
+    # usado na tag base, permite usar caminhos relativos nos menus e demais elementos html
+    # na versão 1 era dashboard_url
+    'app_url' => config('app.url'),
+
+    # login e logout
+    'logout_method' => 'POST',
+    'logout_url' => 'logout',
+    'login_url' => 'login',
+
+    # menus
+    'menu' => $menu,
+    'right_menu' => $right_menu,
+
+    # mensagens flash - https://uspdev.github.io/laravel#31-mensagens-flash
+    'mensagensFlash' => false,
+];
---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,17 @@
+@extends('laravel-usp-theme::master')
+
+@section('title') 
+  @parent 
+@endsection
+
+@section('styles')
+  @parent
+  <style>
+  </style>
+@endsection
+
+@section('javascripts_bottom')
+  @parent
+  <script>
+  </script>
+@endsection
