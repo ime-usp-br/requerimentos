b488a44a35260dcc8566358c7209b0433d8165fe
Guilherme Simões <guilhermesimoesmarin@gmail.com>
2024-02-21 19:01:33 -0300

    feat: adicionar mudanças nas rotas

--- routes/web.php
+++ routes/web.php
@@ -25,9 +25,19 @@ Route::get('/acesso-negado', function() {
 });
 
 Route::get('/lista', function () {
-    $reqs = Requisition::with('takenDisciplines')->select('created_at', 'requested_disc', 'nusp', 'situation', 'id')->where('nusp', Auth::user()->codpes)->get();
 
-    return view('pages.list', ['reqs' => $reqs]);
+    $user = Auth::user();
+
+    if ($user->name === 'Guilherme Simoes Santos Marin') {
+        $reqs = Requisition::with('takenDisciplines')->select('created_at', 'requested_disc', 'nusp', 'situation', 'id')->where('nusp', Auth::user()->codpes)->get();
+
+        return view('pages.list', ['reqs' => $reqs]);
+    } elseif ($user->name === 'João') {
+        $reqs = Requisition::with('takenDisciplines')->select('created_at', 'requested_disc', 'nusp', 'situation', 'id')->where('nusp', Auth::user()->codpes)->get();
+
+        return view('pages.list', ['reqs' => $reqs]);
+    }
+    
 })->middleware('auth')->name('list');
 
 Route::get('/novo-requerimento', function() {
@@ -71,11 +81,8 @@ Route::post('/novo-requerimento', function(Request $request) {
 
     $req = new Requisition;
     $req->department = $data['disc-department'];
-    // $req->nusp = $data['nusp'];
     $req->nusp = Auth::user()->codpes;
-    // $req->student_name = $data['name'];
     $req->student_name = Auth::user()->name;
-    // $req->email = $data['email'];
     $req->email = Auth::user()->email;
     $req->course = $data['course'];
     $req->requested_disc = $data['requested-disc-name'];
@@ -83,7 +90,8 @@ Route::post('/novo-requerimento', function(Request $request) {
     $req->requested_disc_code = $data['requested-disc-code'];
     $req->situation = "Encaminhado para a secretaria";
     $req->reviewer_name = null;
-    $req->result = null;
+    $req->result = 'Sem resultado';
+    $req->result_text = null;
     $req->appraisal = null;
     $req->reviewer_nusp = null;
     $req->reviewer_decision = 'Sem decisão';
@@ -92,6 +100,7 @@ Route::post('/novo-requerimento', function(Request $request) {
     $req->taken_discs_syllabus = $request->file('taken-disc-syllabus')->store('test');
     $req->requested_disc_syllabus = $request->file('requested-disc-syllabus')->store('test');
     $req->observations = $request->observations;
+    $req->validated_by_sg = false;
 
     $req->save();
 
@@ -107,6 +116,6 @@ Route::post('/novo-requerimento', function(Request $request) {
         $takenDisc->save();
     }
 
-    return redirect()->route('newRequisition')->with('success', ['title message' => 'Requerimento criado', 'body message' => 'O requerimento foi criado com sucesso. Acompanhe o andamento pela página inicial.']);
+    return redirect()->route('newRequisition')->with('success', ['title message' => 'Requerimento criado', 'body message' => "O requerimento foi criado com sucesso. Acompanhe o andamento pelo campo 'situação' na página inicial."]);
 
 })->name('requisitions.create');
