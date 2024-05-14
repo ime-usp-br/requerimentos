2c18824c53e5c4872bfadcdc45e059347c8b36c7
Guilherme Simões <guilhermesimoesmarin@gmail.com>
2024-05-14 20:20:28 -0300

    fix: ajustar prioridade dos papéis ao logar

--- app/Http/Controllers/GlobalController.php
+++ app/Http/Controllers/GlobalController.php
@@ -40,12 +40,16 @@ class GlobalController extends Controller
         $user->email = $userSenhaUnica->email ?? $userSenhaUnica->emailUsp ?? $userSenhaUnica->emailAlternativo ?? 'invalido' . $user->codpes . '@usp.br';
         $user->name = $userSenhaUnica->nompes;
 
+
+        // a ordem em que os elementos estão nesse vetor determina a prioridade
+        // que um papel tem quando um usuário com mais de um papel loga no 
+        // sistema 
         $rolesInfo = [[RoleName::REVIEWER, RoleId::REVIEWER, 'sg.list'],
+                      [RoleName::MAP_SECRETARY, RoleId::MAP_SECRETARY, 'department.list', 'map'],
                       [RoleName::SG, RoleId::SG, 'sg.list'],
                       [RoleName::MAC_SECRETARY, RoleId::MAC_SECRETARY, 'department.list', 'mac'],
                       [RoleName::MAT_SECRETARY, RoleId::MAT_SECRETARY, 'department.list', 'mat'],
-                      [RoleName::MAE_SECRETARY, RoleId::MAE_SECRETARY, 'department.list', 'mae'],
-                      [RoleName::MAP_SECRETARY, RoleId::MAP_SECRETARY, 'department.list', 'map']];
+                      [RoleName::MAE_SECRETARY, RoleId::MAE_SECRETARY, 'department.list', 'mae']];
 
         foreach ($rolesInfo as $roleInfo) {
             if ($user->hasRole($roleInfo[0])) {
@@ -55,6 +59,8 @@ class GlobalController extends Controller
                 return redirect()->route($roleInfo[2], ['departmentName' => $roleInfo[3] ?? NULL]);
             }
         }
+
+        // o papel estudante é o default do sistema 
         $user->current_role_id = RoleId::STUDENT;
         $user->save();
         \Auth::login($user, true);
