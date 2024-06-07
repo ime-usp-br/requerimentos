a255565a0911099197a6c4584e29603a3087597a
Guilherme Simões <guilhermesimoesmarin@gmail.com>
2024-06-06 21:46:56 -0300

    fix: ajustar textos dos tipos de evento

--- app/Enums/EventType.php
+++ app/Enums/EventType.php
@@ -4,13 +4,13 @@ namespace App\Enums;
 
 class EventType 
 {
-    const SENT_TO_SG = 'Encaminhado para a secretaria';
+    const SENT_TO_SG = 'Encaminhado para a secretaria pelo aluno';
     const SENT_TO_REVIEWERS = 'Enviado para análise dos pareceristas';
     const BACK_TO_STUDENT = 'Retornado para o aluno devido a inconsistência nos dados';
-    const ACCEPTED = 'Deferido';
-    const REJECTED = 'Indeferido';
+    const ACCEPTED = 'Requerimento deferido';
+    const REJECTED = 'Requerimento indeferido';
     const RETURNED_BY_REVIEWER = 'Retornado por um parecerista';
-    const IN_REVALUATION = 'Em reavaliação';
+    const IN_REVALUATION = 'Requerimento em reavaliação';
     const RESEND_BY_STUDENT = 'Reenviado pelo aluno depois de atualização';
     const SENT_TO_DEPARTMENT = 'Enviado para análise do departamento';
 }
