9c316d3701401330aedce16759deae2328943458
Guilherme Simões <guilhermesimoesmarin@gmail.com>
2024-06-12 22:34:03 -0300

    fix: ajustando a página de detalhe da versão

--- resources/views/pages/records/requisitionVersion.blade.php
+++ resources/views/pages/records/requisitionVersion.blade.php
@@ -2,25 +2,26 @@
 
 @section('head')
     
-    <link rel="stylesheet" type="text/css" href="{{ asset('css/pages/sg/detail.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{ asset('css/pages/records/detail.css') }}">
     <script src="{{ asset('js/department/detail.js')}}" defer></script>
 
     <!-- ionicons -->
     <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
     <script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
-    <title>Detalhes do requerimento</title>
+    <title>Detalhes da versão</title>
 @endsection
 
 @section('content')
     <div class="content">
         <header>
-            <h1>Detalhes do requerimento {{ $req->id }} </h1>
+            <h1>Detalhes da versão </h1>
+            <a href="{{ route('record.requisition', ['requisitionId' => $event->requisition_id ]) }}" class="button">Voltar</a>
         </header>
         
-        <nav class="nav">
-            <a href="{{ route('record.versionReviews', ['eventId' =>  $event->id ]) }}" class="button">Pareceres</a>
-            <a href="{{ route('record.requisition', ['requisitionId' => $event->requisition_id ]) }}" class="button">Voltar</a>
-        </nav>
+        {{--<nav class="nav">
+             <a href="{{ route('record.versionReviews', ['eventId' =>  $event->id ]) }}" class="button">Pareceres</a> 
+        </nav>--}}
+        <p>Abaixo temos os dados do requerimento registrados no dia {{ $event->created_at->format('d/m/Y') }}, às {{ $event->created_at->format('H:i') }}</p>
 
         <form method="GET" action="{{ route('reviewer.reviewerPick', ['requisitionId' => $req->id])}}" id="form" >
             @csrf
