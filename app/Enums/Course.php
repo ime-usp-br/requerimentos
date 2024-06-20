a7071bb2ca2b21284cbd019bb5b39389979ead51
Guilherme Simões <guilhermesimoesmarin@gmail.com>
2024-06-20 20:38:54 -0300

    refactor: adicionar novos enums

---
+++ app/Enums/Course.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Enums;
+
+class Course 
+{
+    const BCC = 'Bacharelado em Ciência da Computação';
+    const STATISTICS = 'Bacharelado em Estatística';
+    const MAT_LIC = 'Licenciatura em Matemática';
+    const MAT_PURE = 'Bacharelado em Matemática';
+    const MAT_COMP_APPLIED = 'Bacharelado em Matemática Aplicada e Computacional';
+    const MAT_APPLIED = 'Bacharelado em Matemática Aplicada';
+}
