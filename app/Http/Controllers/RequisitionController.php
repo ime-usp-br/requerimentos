16ca17b76cae06bd3249a615ad97bb37e33d851d
soueuabianca <150519789+soueuabianca@users.noreply.github.com>
2024-07-23 07:04:45 -0300

    Add files via upload

---
+++ app/Http/Controllers/RequisitionController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Requisition;
+use Symfony\Component\HttpFoundation\StreamedResponse;
+
+class RequisitionController extends Controller
+{
+    public function exportCSV()
+    {
+        $requisitions = Requisition::all();
+
+        $response = new StreamedResponse(function() use ($requisitions) {
+            $handle = fopen('php://output', 'w');
+
+            fputcsv($handle, ['Aluno', 'Número USP', 'Códido da disciplina', 'Tipo da disciplina', 'Situação']);
+
+            foreach ($requisitions as $requisition) {
+                fputcsv($handle, [                    
+                    $requisition->student_name,
+                    $requisition->nusp,
+                    $requisition->requested_disc_code,
+                    $requisition->requested_disc_type,
+                    $requisition->internal_status,
+                    
+                ]);
+            }
+
+            fclose($handle);
+        });
+
+        $response->headers->set('Content-Type', 'text/csv');
+        $response->headers->set('Content-Disposition', 'attachment; filename="requisitions.csv"');
+
+        return $response;
+    }
+}
