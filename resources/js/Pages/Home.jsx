dd67c429d9387d5c98768ac135516ae16cdec238
Teruya11 <daiki.teruya@usp.br>
2025-02-01 15:36:50 -0300

    feat: Adicionado uma página inicial

--- resources/js/Pages/Home.jsx
+++ resources/js/Pages/Home.jsx
@@ -1,17 +1,25 @@
 import React from 'react';
-import { Head } from '@inertiajs/react'
-import styled from 'styled-components'
 
-const H1 = styled.h1`
-    color: blue;
-`;
+import '../../../public/css/global.css';
+import '../../../public/css/pages/home.css';
+import '../../../public/css/components/footer.css';
+import '../../../public/css/components/overlay.css';
+import '../../../public/css/components/table.css';
 
 export default function Home() {
     return (
-        <div>
-            <Head title="Welcome" />
-            <H1>Welcome</H1>
-            <p>Hello boy, welcome to your first Inertia app!</p>
-        </div>
+        <>
+            <header>
+                <img src="https://requerimentos.ime.usp.br/img/home/ime-logo-title.svg" />
+                <a href="login" class="button">⎆ Acessar</a>
+            </header>
+
+            <footer>
+                <div class="footer-content">
+                    <img class="ime-logo" src="img/footer/ime-logo-footer.svg" />
+                    <img class="usp-logo" src="img/footer/usp-logo.svg" />
+                </div>
+            </footer>
+        </>
     );
 };
