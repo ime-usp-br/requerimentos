0f0a60e1728145a4b9e6dd6186b3c5f091cd4f76
Teruya11 <daiki.teruya@usp.br>
2025-02-03 11:20:35 -0300

    fix: correção para fazer route() funcionar

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,8 +4,8 @@
 <head>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
-    @routes
     @vite('resources/js/app.jsx')
+    @routes
     @inertiaHead
 </head>
 
