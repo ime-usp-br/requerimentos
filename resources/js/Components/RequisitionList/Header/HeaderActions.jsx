aee8ba628daba764def0a8a1a2279cc7290c603f
Teruya11 <daiki.teruya@usp.br>
2025-02-10 08:44:03 -0300

    feat: O menu de 'roles' do header agora funciona

--- resources/js/Components/RequisitionList/Header/HeaderActions.jsx
+++ resources/js/Components/RequisitionList/Header/HeaderActions.jsx
@@ -1,10 +1,25 @@
-import React, { useRef } from 'react'
+import React from 'react';
 import { Stack, Button, Box } from '@mui/material';
-
 import ComboBox from '../../ComboBox';
 
 export default function HeaderActions({ roleId, userRoles }) {
-    const formRef = useRef(null);
+    const handleComboBoxChange = (value) => {
+        const csrfToken = document.querySelector('meta[name="csrf-token"]').getAttribute('content');
+
+        fetch(route('role.switch'), {
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json',
+                'X-CSRF-TOKEN': csrfToken
+            },
+            body: JSON.stringify({
+                'role-switch': value.id
+            })
+        }).then(response => {
+            window.location.href = response.url;
+        });
+    };
 
     return (
         <Stack 
@@ -14,32 +29,18 @@ export default function HeaderActions({ roleId, userRoles }) {
                 alignItems: "center"
             }}
             spacing={2}
-            >
-            
-            { roleId > 1 && userRoles.length > 1 ? 
-                <Box
-                    component="form"
-                    ref={formRef}
-                    onSubmit={(e) => {
-                    e.preventDefault();
-                    const formData = new FormData(e.target);
-                    const data = Object.fromEntries(formData.entries());
-                    console.log("Form submitted: ", data);
-                    }}    
-                >
-                    <ComboBox
-                        size='small'
-                        options={userRoles}
-                        optionGetter={(option) => option.name}
-                        defaultValue={userRoles.find(val => val.id == roleId)}
-                        sx={{ 
-                            width: 230
-                        }}
-                        name='papel'
-                    />
-                </Box>
-                : null 
-            }
+        >
+            { roleId > 1 && userRoles.length > 1 && (
+                <ComboBox
+                    size='small'
+                    options={userRoles}
+                    optionGetter={(option) => option.name}
+                    defaultValue={userRoles.find(val => val.id == roleId)}
+                    sx={{ width: 230 }}
+                    name='papel'
+                    onChange={handleComboBoxChange}
+                />
+            )}
             <Button 
                 variant="contained" 
                 size="large"
--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,6 +4,7 @@
 <head>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
+    <meta name="csrf-token" content="{{ csrf_token() }}">
     @vite('resources/js/app.jsx')
     @routes
     @inertiaHead
