55f64c6edc7055e75f0d0661b0579891319b888a
Teruya11 <daiki.teruya@usp.br>
2025-02-21 14:13:18 -0300

    fix: A página de login foi corrigida com Material

--- resources/js/Pages/Home.jsx
+++ resources/js/Pages/Home.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import LoginIcon from '@mui/icons-material/Login';
+import { Stack, Button } from '@mui/material';
 
 // import '../../../public/css/global.css';
 // import '../../../public/css/pages/home.css';
@@ -9,18 +10,69 @@ import LoginIcon from '@mui/icons-material/Login';
 
 export default function Home() {
     return (
-        <div className='home-body'>
-            <header className='home-header'>
-                <img className='archimedes' src="https://requerimentos.ime.usp.br/img/home/ime-logo-title.svg" />
-                <a href="login" class="home-button"><LoginIcon /> Acessar</a>
-            </header>
+        <Stack
+            direction='column'
+            sx={{
+                alignItems: 'center',
+                justifyContent: 'space-between',
+                backgroundImage: "url('https://requerimentos.ime.usp.br/img/home/background.png')",  
+                backgroundSize: 'cover',
+                backgroundColor: 'black',
+                width: '100%',
+                height: '100vh',
+            }}
+        >
+            <Stack 
+                direction="row"
+                sx={{ 
+                    alignItems: "center",
+                    justifyContent: "center",
+                    width: '100%',
+                    height: 180,
+                }}
+            >
+                <Stack
+                    direction='row'
+                    sx={{ 
+                        justifyContent: "space-between",
+                        width: '85%',
+                        height: 'auto'
+                    }}
+                >
+                    <img style={{ transform: 'scale(1.3)' }} src="https://requerimentos.ime.usp.br/img/home/ime-logo-title.svg" />
+                    <Button 
+                        variant="contained" 
+                        size="large"
+                        color="primary" 
+                        href="login"
+                    >
+                        <LoginIcon /> &nbsp; Acessar
+                    </Button>
+                </Stack>
+            </Stack>
 
-            <footer className='home-footer'>
-                <div class="home-footer-content">
+            <Stack
+                direction='row'
+                sx={{ 
+                    alignItems: "center",
+                    justifyContent: "center",
+                    width: '100%',
+                    height: 140,
+                    backgroundColor: 'primary.main',
+                }}
+            >
+                <Stack
+                    direction='row'
+                    sx={{ 
+                        justifyContent: "space-between",
+                        width: '90%',
+                        height: 'auto'
+                    }}
+                >
                     <img class="ime-logo" src="img/footer/ime-logo-footer.svg" />
                     <img class="usp-logo" src="img/footer/usp-logo.svg" />
-                </div>
-            </footer>
-        </div>
+                </Stack>
+            </Stack>
+        </Stack>
     );
 };
