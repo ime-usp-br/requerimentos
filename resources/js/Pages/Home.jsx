d692a15c787737f9e93211060b9d201518ddaddc
Teruya11 <daiki.teruya@usp.br>
2025-02-23 18:21:18 -0300

    fix: Ajustes pequenos no layout da Home

--- resources/js/Pages/Home.jsx
+++ resources/js/Pages/Home.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import LoginIcon from '@mui/icons-material/Login';
-import { Stack, Button } from '@mui/material';
+import { Stack, Button, Container } from '@mui/material';
 
 // import '../../../public/css/global.css';
 // import '../../../public/css/pages/home.css';
@@ -15,62 +15,98 @@ export default function Home() {
             sx={{
                 alignItems: 'center',
                 justifyContent: 'space-between',
-                backgroundImage: "url('https://requerimentos.ime.usp.br/img/home/background.png')",  
+                backgroundImage: "url('https://requerimentos.ime.usp.br/img/home/background.png')",
                 backgroundSize: 'cover',
                 backgroundColor: 'black',
                 width: '100%',
                 height: '100vh',
             }}
         >
-            <Stack 
-                direction="row"
-                sx={{ 
+            <Stack
+                direction={{ xs: 'column', sm: 'row' }}
+                spacing={{ xs: 3 }}
+                sx={{
                     alignItems: "center",
-                    justifyContent: "center",
-                    width: '100%',
-                    height: 180,
+                    justifyContent: "space-between",
+                    width: '90%',
+                    height: { xs: 'auto', sm: 160 },
+                    paddingTop: 3
                 }}
             >
-                <Stack
-                    direction='row'
-                    sx={{ 
-                        justifyContent: "space-between",
-                        width: '85%',
-                        height: 'auto'
+                <Container
+                    sx={{
+                        height: 'auto',
+                        width: { xs: 320, sm: 460 }
                     }}
                 >
-                    <img style={{ transform: 'scale(1.3)' }} src="https://requerimentos.ime.usp.br/img/home/ime-logo-title.svg" />
-                    <Button 
-                        variant="contained" 
-                        size="large"
-                        color="primary" 
-                        href="login"
-                    >
-                        <LoginIcon /> &nbsp; Acessar
-                    </Button>
-                </Stack>
+                    <img
+                        style={{
+                            width: '100%'
+                        }}
+                        src="https://requerimentos.ime.usp.br/img/home/ime-logo-title.svg"
+                    />
+                </Container>
+                <Button
+                    variant="contained"
+                    size="large"
+                    color="primary"
+                    href="login"
+                    style={{ textAlign: 'center' }}
+                >
+                    <LoginIcon /> Acessar
+                </Button>
             </Stack>
 
             <Stack
                 direction='row'
-                sx={{ 
+                sx={{
                     alignItems: "center",
                     justifyContent: "center",
                     width: '100%',
-                    height: 140,
+                    height: { xs: 100, sm: 140 },
                     backgroundColor: 'primary.main',
                 }}
             >
                 <Stack
                     direction='row'
-                    sx={{ 
+                    sx={{
                         justifyContent: "space-between",
                         width: '90%',
                         height: 'auto'
                     }}
                 >
-                    <img class="ime-logo" src="img/footer/ime-logo-footer.svg" />
-                    <img class="usp-logo" src="img/footer/usp-logo.svg" />
+                    <Container
+                        disableGutters
+                        sx={{
+                            alignContent: 'center',
+                            height: 'auto',
+                            width: { xs: 200, sm: 460 },
+                            margin: 0
+                        }}
+                    >
+                        <img
+                            style={{
+                                width: '100%'
+                            }}
+                            src="img/footer/ime-logo-footer.svg"
+                        />
+                    </Container>
+                    <Container
+                        disableGutters
+                        sx={{
+                            alignContent: 'center',
+                            height: 'auto',
+                            width: { xs: 110, sm: 170 },
+                            margin: 0
+                        }}
+                    >
+                        <img
+                            style={{
+                                width: '100%'
+                            }}
+                            src="img/footer/usp-logo.svg"
+                        />
+                    </Container>
                 </Stack>
             </Stack>
         </Stack>
