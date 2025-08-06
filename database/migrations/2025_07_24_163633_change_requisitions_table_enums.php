ac6da78aa6e1e1bd394a424aa065a36effc44f2d
Kim <kimkakeya@gmail.com>
2025-08-05 21:54:57 -0300

    refactor: update enum definition for events and enhance down migration for requisitions

--- database/migrations/2025_07_24_163633_change_requisitions_table_enums.php
+++ database/migrations/2025_07_24_163633_change_requisitions_table_enums.php
@@ -27,9 +27,13 @@ return new class extends Migration
     /**
      * Reverse the migrations.
      */
-    public function down(): void
-    {
-        Schema::table('requisitions', function (Blueprint $table) {
-        });
-    }
+public function down(): void
+{
+    Schema::table('requisitions', function (Blueprint $table) {
+        $table->enum('department', ['MAC', 'MAE', 'MAT', 'MAP', 'Disciplina de fora do IME'])->change();
+        $table->enum('course', ['Bacharelado em Ciência da Computação', 'Bacharelado em Estatística', 'Bacharelado em Matemática', 'Bacharelado em Matemática Aplicada', 'Bacharelado em Matemática Aplicada e Computacional', 'Licenciatura em Matemática'])->change();
+        $table->enum('requested_disc_type', ['Extracurricular', 'Obrigatória', 'Optativa Eletiva', 'Optativa Livre'])->change();
+        $table->enum('result', ['Sem resultado', 'Inconsistência nas informações', 'Deferido', 'Indeferido'])->change();
+    });
+}
 };
