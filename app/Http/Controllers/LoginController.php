5c78e7fc1716037d69d9e1fa37e0f174123700a9
Kim <kimkakeya@gmail.com>
2025-08-28 11:11:32 -0300

    feat: Utilizando replicado para popular informações do usuário quando ele recebe uma role

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -24,23 +24,25 @@ class LoginController extends Controller
 		Log::info('User login attempt: ', ['user_data' => json_encode($userSenhaUnica->user ?? $userSenhaUnica, JSON_PRETTY_PRINT)]);
 
 		// $fromIME = false;
-        // foreach ($userSenhaUnica->vinculo as $vinculo){
-        //     if (isset($vinculo["siglaUnidade"]) && $vinculo["siglaUnidade"] === 'IME') {
-        //         $fromIME = true;
-        //         break; 
-        //     }
-        // }
-        
-        // if (!$fromIME) {
-        //     abort(403, 'Acesso negado. Seu vínculo com o IME não foi encontrado. Se você for aluno do IME, entre em contato com o serviço de graduação.');
-        // }
+		// foreach ($userSenhaUnica->vinculo as $vinculo){
+		//     if (isset($vinculo["siglaUnidade"]) && $vinculo["siglaUnidade"] === 'IME') {
+		//         $fromIME = true;
+		//         break; 
+		//     }
+		// }
+
+		// if (!$fromIME) {
+		//     abort(403, 'Acesso negado. Seu vínculo com o IME não foi encontrado. Se você for aluno do IME, entre em contato com o serviço de graduação.');
+		// }
 
 		$user = User::where('codpes', $userSenhaUnica->codpes)->first();
 		if (is_null($user)) {
 			$user = DB::transaction(function () use ($userSenhaUnica) {
 				$user = new User;
 				$user->codpes = $userSenhaUnica->codpes;
-				$user->email = $userSenhaUnica->email ?? $userSenhaUnica->emailUsp ?? $userSenhaUnica->emailAlternativo ?? 'invalido' . $userSenhaUnica->codpes . '@usp.br';
+				$user->email = $userSenhaUnica->emailUsp ?? $userSenhaUnica->email
+					?? $userSenhaUnica->emailAlternativo
+					?? 'invalido' . $userSenhaUnica->codpes . '@usp.br';
 				$user->name = $userSenhaUnica->nompes;
 				$user->current_role_id = RoleId::STUDENT;
 				$user->current_department_id = null;
@@ -49,11 +51,18 @@ class LoginController extends Controller
 				return $user;
 			});
 		}
-		// Se o usuário nunca logou, mas recebeu uma role, precisamos dar a ele nome, email e a role estudante.
+
+		// O usuário pode ter sido cadastrado por meio de uma atribuição de role
+		// Nesse caso, o email dele estará nulo, e teremos que completar as informações
+		// Se o replicado não tiver encontrado o usuário, é possível que o nome esteja nulo também
 		else if (is_null($user->email) || is_null($user->name)) {
 			DB::transaction(function () use ($user, $userSenhaUnica) {
-				$user->email = $userSenhaUnica->email ?? $userSenhaUnica->emailUsp ?? $userSenhaUnica->emailAlternativo ?? 'invalido' . $user->codpes . '@usp.br';
-				$user->name = $userSenhaUnica->nompes;
+				$user->email = $user->email ?? $userSenhaUnica->emailUsp
+					?? $userSenhaUnica->email
+					?? $userSenhaUnica->emailAlternativo
+					?? 'invalido' . $user->codpes . '@usp.br';
+				$user->name = $user->name ?? $userSenhaUnica->nompes;
+
 				$user->save();
 				$user->assignRole(RoleId::STUDENT);
 			});
